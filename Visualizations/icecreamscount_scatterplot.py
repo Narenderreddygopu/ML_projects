c42f84491e4576594cdb0b1a281837703b529c6c
Narenderreddygopu <narendergopu25@gmail.com>
2023-06-29 10:41:00 -0500

    Update icecreamscount_scatterplot.py
    CC

--- Visualizations/icecreamscount_scatterplot.py
+++ Visualizations/icecreamscount_scatterplot.py
@@ -1,9 +1,11 @@
+#Importing package matplotlib
 import matplotlib.pyplot as plt
 
 temp = [30, 32, 33, 28.5, 35, 29, 29]
 ice_creams_count = [100, 115, 115, 75, 125, 79, 89]
 
 plt.scatter(temp, ice_creams_count)
+#labeling the co-ordinates with title name
 plt.title("Temperature vs. Sold ice creams")
 plt.xlabel("Temperature")
 plt.ylabel("Sold ice creams count")
